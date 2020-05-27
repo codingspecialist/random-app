148d2fceb2a66ef2d3e10e14997c4375617ef9f4
ssar <ssar@nate.com>
2020-05-27 14:57:11 +0900

    random app

---
+++ src/random/App.java
@@ -0,0 +1,11 @@
+package random;
+
+import java.util.Random;
+
+public class App {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int num = r.nextInt(50)+1;
+		System.out.println("random value : "+num);
+	}
+}
